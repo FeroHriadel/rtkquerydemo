b66a29ff3fb1ccb438de4fac20a3054abf0a78fb
Fero Hriadel <ferdinand.hriadel@gmail.com>
2025-01-17 16:47:05 +0100

    overode trashButton

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
   const initialViewState = { latitude: 49.212132, longitude: 16.598509, zoom: 14 };
   const [mapLoaded, setMapLoaded] = useState<boolean>(false);
+  const drawRef = useRef<MapboxDraw | null>(null);
   const mapRef = useRef<MapRef | null>(null);
   const azimuthFeaturesRef = useRef<GeoJSON.Feature<GeoJSON.Point>[]>([]);
   const { calculateLineLength, calculateAzimuths, drawAzimuths, updateAzimuthLayer } = useMapCalcs({ mapRef, azimuthFeaturesRef });
@@ -18,7 +19,7 @@ function App() {
   const getMap = () => mapRef.current?.getMap();
 
   // Initializes the drawing controls
-  const initDraw = () => new MapboxDraw({
+  const initDraw = () => drawRef.current = new MapboxDraw({
     displayControlsDefault: false,
     controls: { line_string: true, trash: true },
   });
@@ -33,22 +34,23 @@ function App() {
   };
 
   // Clears any previous azimuths
-  const clearAzimuths = () => {
+  const clearAzimuths = useCallback(() => {
     azimuthFeaturesRef.current = [];
-  };
+  }, []);
+  
 
   // Remove all drawn lines, keeping the last one if more than one exists
-  const removeDrawings = (draw: MapboxDraw) => {
-    const allFeatures = draw.getAll();
+  const removeDrawings = () => {
+    const allFeatures = drawRef.current!.getAll();
     if (allFeatures.features.length > 1) {
-      draw.delete(allFeatures.features[0].id as string);
+      drawRef.current!.delete(allFeatures.features[0].id as string);
     }
   };
 
   // Event handlers for draw actions
   const onDrawCreate = useCallback(
-    (event: { features: GeoJSON.Feature<GeoJSON.Geometry>[] }, draw: MapboxDraw) => {
-      removeDrawings(draw);
+    (event: { features: GeoJSON.Feature<GeoJSON.Geometry>[] }) => {
+      removeDrawings();
       const feature = event.features[0];
       if (feature && feature.geometry.type === "LineString") {
         clearAzimuths();
@@ -56,7 +58,7 @@ function App() {
         drawAzimuths(feature.geometry);
       }
     },
-    [calculateLineLength, drawAzimuths]
+    [calculateLineLength, drawAzimuths, clearAzimuths]
   );
 
   const onDrawUpdate = useCallback(
@@ -72,31 +74,60 @@ function App() {
     [calculateLineLength, calculateAzimuths, drawAzimuths]
   );
 
-  const onDrawDelete = useCallback(() => {
-    clearAzimuths();
-    updateAzimuthLayer();
-  }, [updateAzimuthLayer]);
-
   const addDrawListeners = useCallback(
-    (map: mapboxgl.Map, draw: MapboxDraw) => {
-      map.on("draw.create", (event) => onDrawCreate(event, draw));
+    (map: mapboxgl.Map) => {
+      map.on("draw.create", (event) => onDrawCreate(event));
       map.on("draw.update", onDrawUpdate);
-      map.on("draw.delete", onDrawDelete);
+      // map.on("draw.delete", clearAzimuths);  =>  trash button malfunctioned, I overode it in maskTrashButton and clearMap
     },
-    [onDrawCreate, onDrawUpdate, onDrawDelete]
+    [onDrawCreate, onDrawUpdate]
   );
 
+  // Create a new invisible element over the trash button
+  const maskTrashButton = (deleteLineBtn: HTMLElement) => {
+    const overlayDiv = document.createElement("div");
+      overlayDiv.style.position = "absolute";
+      overlayDiv.style.top = `${deleteLineBtn.getBoundingClientRect().top}px`;
+      overlayDiv.style.left = `${deleteLineBtn.getBoundingClientRect().left}px`;
+      overlayDiv.style.width = `${deleteLineBtn.offsetWidth}px`;
+      overlayDiv.style.height = `${deleteLineBtn.offsetHeight}px`;
+      overlayDiv.style.backgroundColor = "transparent";
+      overlayDiv.style.zIndex = "999";
+      document.body.appendChild(overlayDiv);
+      return overlayDiv;
+  }
 
+  //Remove everything painted in map
+  const clearMap = useCallback(() => {
+    if (drawRef.current) {
+      drawRef.current.deleteAll();
+      clearAzimuths();
+      updateAzimuthLayer();
+    }
+  }, [clearAzimuths, updateAzimuthLayer]);
+ 
+
+  // Init MapboxDraw
   useEffect(() => {
     if (!mapLoaded) return;
     const map = getMap();
-    const draw = initDraw();
-    addDrawToMap(map!, draw);
-    addDrawListeners(map!, draw);
+    initDraw();
+    addDrawToMap(map!, drawRef.current!);
+    addDrawListeners(map!);
     return () => {
-      if (map) map.removeControl(draw);
+      if (map) map.removeControl(drawRef.current!);
     };
   }, [mapLoaded, addDrawListeners]);
+
+  // Overide MapboxDraw trash button
+  useEffect(() => {
+    if (!mapLoaded) return;
+    const deleteLineBtn = document.querySelector(".mapbox-gl-draw_trash") as HTMLElement;
+    if (!deleteLineBtn) return;
+    const overlayDiv = maskTrashButton(deleteLineBtn);
+    overlayDiv.addEventListener("click", clearMap);
+    return () => { overlayDiv.removeEventListener("click", clearMap); document.body.removeChild(overlayDiv); }
+  }, [mapLoaded, clearMap]);
 
 
   return (
